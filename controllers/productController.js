72c5870134483568f6b09901f95e8d5dd29a0619
franciszzzzzz <uchennaokonkwo83@gmail.com>
2026-07-10 13:30:22 +0100

    getProductsByGroup changed to get 10 products per category

--- controllers/productController.js
+++ controllers/productController.js
@@ -330,7 +330,7 @@ export const getProductsByGroup = handleAsyncError(async (req, res, next) => {
       wc.get("/products", {
         params: {
           category: id,
-          per_page: 8,
+          per_page: 10,
         },
       }),
     ),
